e22f2db21a92cbf8e210b100f9306d8b29a2c7fb
Rebecca Ariss <rebeccamarionruth@gmail.com>
2022-01-15 12:12:30 -0500

    Basic endpoint setup and signin route testing paramaters

--- server.js
+++ server.js
@@ -1,17 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+// "database" for initial endpoint tests:
+const database = {
+    users: [
+        {
+            id: '1',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            savedCharacters: 0,
+            characterNames: [],
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
     res.send('this is working');
 })
 
+//note that res.json is a method that comes with express and will return a message in quotations as opposed to res.send which does not. visual distinction.
 app.post('/signin', (req, res) => {
-    res.json('signed in');
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('signed in');
+    } else {
+        res.status(400).json('could not sign in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    res.json('registered');
+})
+
+app.get('/profile/:userId', (req, res) => {
+    res.json('on user profile');
+})
+
+app.put('/list/:npcId', (req, res) => {
+    res.json('added NPC to list');
+})
+
+app.delete('/list/:npcId', (req, res) => {
+    res.json('removed NPC from list');
 })
 
 app.listen(3000, () => {
-    console.log('app is running on port 3000')
+    console.log('app is running on port 3000');
 })
 
 // API Endpoint Plannning:
@@ -20,3 +59,4 @@ app.listen(3000, () => {
 // /register --> POST = user
 // /profile/:userId --> GET = user
 // PUT request for updating saved NPCs
+// DELETE for removing NPCs from saved list
